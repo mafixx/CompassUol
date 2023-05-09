f6674ab83553c2d651bcac0a064513795184f124
PEDRO BARBIERI <88338095+mafixx@users.noreply.github.com>
2023-05-09 19:08:27 -0300

    Add files via upload

---
+++ Sprint 7/D&A AWS 7.10/Tarefa 3/boto.py
@@ -0,0 +1,28 @@
+import boto3
+import os
+
+# Definição das variáveis
+aws_access_key_id = "ASIA4JZ2XODUPF7WE2NN"
+aws_secret_access_key = "FKa7Ql/+6bPCKn2vcW8cUkYDxuOgwHlQkSeNAZS9"
+bucket_name = "tarefa3"
+raw_zone = "Raw"
+local = "Local"
+csv = "CSV"
+movies = "Movies"
+series = "Series"
+current_date = "2022/05/02"
+movies_file_path = "movies.csv"
+series_file_path = "series.csv"
+
+# Configuração do cliente S3
+s3 = boto3.client("s3",
+                  aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key)
+
+# Upload dos arquivos CSV para o S3
+s3.upload_file(movies_file_path,
+               bucket_name,
+               os.path.join(raw_zone, local, csv, movies, current_date.replace("/", "\\"), "movies.csv"))
+s3.upload_file(series_file_path,
+               bucket_name,
+               os.path.join(raw_zone, local, csv, series, current_date.replace("/", "\\"), "series.csv"))
