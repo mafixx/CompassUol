ecfaaf5b2ae1fd92a5d211dcca86844e9317efe8
Pedro Henrique <pedroh@localhost.localdomain>
2023-05-30 10:35:11 -0300

    update

--- Sprint 8/Tarefa 3/rndNms.py
+++ Sprint 8/Tarefa 3/rndNms.py
@@ -1,56 +1,53 @@
 import random
+import time
+import os
 import names
-from pyspark.sql import SparkSession
+import csv
 
-# [Warm up] Reverse em uma lista de inteiros
-int_list = random.sample(range(1000), 250)
-int_list.reverse()
-print(int_list)
+# [Warm up] Lista reversa de 250 inteiros aleatórios
 
-# [Warm up] Ordenação e gravação em arquivo CSV
-countries = ['Argentina', 'Alemanha', 'Austrália', 'Áustria', 'Bélgica', 'Brasil', 'Canadá',
-             'Chile', 'Croácia', 'Cuba', 'República Tcheca', 'Dinamarca', 'Egito', 'Estados Unidos',
-             'Estônia', 'Finlândia', 'França', 'Grécia', 'Islândia', 'Índia', 'Indonésia', 'Inglaterra',
-             'Irlanda', 'Israel', 'Itália', 'Japão', 'Coreia do Norte', 'Coreia do Sul', 'Líbano', 'Luxemburgo',
-             'Malta', 'México', 'Mônaco', 'Montenegro', 'Marrocos', 'Holanda', 'Nova Zelândia', 'Noruega',
-             'Paraguai', 'Peru', 'Polônia', 'Portugal', 'Qatar', 'Romênia', 'Rússia',
-             'Singapura', 'Eslováquia', 'Eslovênia', 'Espanha', 'Suécia', 'Suíça', 'Tailândia', 'Tunísia',
-             'Turquia', 'Ucrânia', 'Uruguai', 'Vaticano', 'Venezuela', 'Vietnã']
+random_integers = [random.randint(1, 100) for _ in range(250)]
+random_integers.reverse()
+print(random_integers)
 
-random.shuffle(countries)
+# [Warm up] 20 nomes de animais
 
-print(countries)
+animal_names = ["Elefante", "Tigre", "Leão", "Girafa", "Zebra", "Macaco", "Urso", "Hipopótamo", "Rinoceronte", "Canguru",
+                "Cobra", "Panda", "Pinguim", "Jacaré", "Arara", "Cachorro", "Camelo", "Lobo", "Raposa", "Falcão"]
 
-countries.sort()
-print(countries)
+sorted_animal_names = sorted(animal_names)
+[print(animal) for animal in sorted_animal_names]
 
-spark = SparkSession.builder.appName("NomeAleatorio").getOrCreate()
+# Salvando o nome dos animais em CSV
 
-# [Laboratório] Geração de nomes aleatórios
+filename = "animal_names.csv"
+with open(filename, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows([[animal] for animal in sorted_animal_names])
+
+print(f"Animal names saved to {filename}")
+
+# [Laboratório] Gerando dataset de nomes aleatórios
+
 random.seed(40)
 qtd_nomes_unicos = 3000
 qtd_nomes_aleatorios = 10000000
 
-aux = [names.get_full_name() for x in range(qtd_nomes_unicos)]
-print("Gerando {} nomes aleatórios".format(qtd_nomes_aleatorios))
+aux = []
+for i in range(qtd_nomes_unicos):
+    aux.append(names.get_full_name())
 
-dados = [random.choice(aux) for x in range(qtd_nomes_aleatorios)]
+print(f"Generating {qtd_nomes_aleatorios} random names")
 
-# Gerar um arquivo de texto com os nomes aleatórios
-nome_arquivo = "nomes_aleatorios.txt"
-with open(nome_arquivo, 'w') as file:
-    file.writelines('\n'.join(dados))
+dados = [random.choice(aux) for _ in range(qtd_nomes_aleatorios)]
 
-# Ler o arquivo de texto como um DataFrame do Spark
-nomes_df = spark.read.text(nome_arquivo)
+# Salvando os nomes aleatórios em um arquivo .txt
 
-# Exibir o conteúdo do DataFrame
-nomes_df.show()
+filename = "nomes_aleatorios.txt"
+with open(filename, "w") as file:
+    file.write("\n".join(dados))
 
-# Salvar o DataFrame como um arquivo CSV em um diretório
-nome_diretorio_csv = "diretorio_csv"
-nomes_df.write.csv(nome_diretorio_csv, header=True)
+print(f"Random names saved to {filename}")
 
-# Verificar o conteúdo do arquivo CSV
-nomes_df_lido = spark.read.csv(nome_diretorio_csv, header=True)
-nomes_df_lido.show()
+# Checando o conteúdo do arquivo criado
+os.system(f"notepad.exe {filename}")
